70c92ee4013fdcfe107c44d6a2384bade0915ae1
Tom Booth <tombooth@gmail.com>
2014-01-29 13:32:38 +0000

    Add basic project setup

---
+++ README.md
@@ -0,0 +1,9 @@
+# Blinken
+
+A dashboard to integrate multiple alert systems
+
+## License
+
+Crown Copyright
+
+Distributed under the Open Government License v2
---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject govuk/blinken "0.1.0-SNAPSHOT"
+  :description "Dashboard to integrate multiple alert systems"
+  :url "https://github.com/alphagov/blinken"
+  :license {:name "Open Government License"
+            :url "http://www.nationalarchives.gov.uk/doc/open-government-licence/version/2/"}
+  :dependencies [[org.clojure/clojure "1.5.1"]])
